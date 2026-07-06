819990dae47e3995c567f202281fd73f268f5dba
DaFrankort <daniel.frankort@telenet.be>
2026-07-06 11:31:39 +0200

    Add reusable logic from https://github.com/DaFrankort/lenny-dnd-bot

---
+++ grimoire/logic/methods.py
@@ -0,0 +1,63 @@
+import logging
+from collections.abc import Callable
+from enum import Enum
+from typing import Any, TypeVar
+
+import discord
+import thread
+import validators
+from PIL import ImageFont
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+
+def when(condition: bool | str | int | None, value_on_true: T, value_on_false: U) -> T | U:
+    """Wrapper method for a ternary statement, for readability"""
+    return value_on_true if condition else value_on_false
+
+
+class FontType(str, Enum):
+    MONOSPACE = "./assets/fonts/GoogleSansCode-Light.ttf"
+    FANTASY = "./assets/fonts/Merienda-Light.ttf"
+
+
+def get_font(font: FontType, size: float):
+    try:
+        return ImageFont.truetype(font=font, size=size)
+    except OSError:
+        logging.warning("Font '%s' could not be loaded!", font)
+        return ImageFont.load_default(size=size)
+
+
+class ChoicedEnum(Enum):
+    @classmethod
+    def choices(cls) -> list[discord.app_commands.Choice[str]]:
+        return [discord.app_commands.Choice(name=e.name.replace("_", " ").title(), value=e.value) for e in cls]
+
+    @classmethod
+    def options(cls) -> list[discord.SelectOption]:
+        return [discord.SelectOption(label=e.value.title(), value=e.value) for e in cls]
+
+    @classmethod
+    def values(cls) -> list[Any]:
+        return [e.value for e in cls]
+
+
+def is_valid_url(url: str) -> bool:
+    try:
+        return bool(validators.url(url))
+    except validators.utils.ValidationError:
+        return False
+
+
+def call_with_timeout(timeout: int, func: Callable[..., T], args: list[Any]) -> T | None:
+    proc = thread.Thread(target=func, args=args)
+    proc.start()
+    proc.join(timeout=timeout)
+
+    if proc.is_alive():
+        proc.kill()
+        return None
+
+    return proc.result
